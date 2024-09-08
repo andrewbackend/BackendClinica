c7fc4fb5e89813865cb2a14bb31bd6dddfa74789
arturoap31 <arturoapbarrios@hotmail.com>
2024-09-08 09:53:29 -0500

    arturo

--- js/script.js
+++ js/script.js
@@ -8,6 +8,15 @@ window.addEventListener('load', () => {
     const email = document.getElementById('patientEmail');
     form.patientPhone.addEventListener("keypress", soloNumeros, false);
 
+    // ARTURO 
+    const fechaCita = document.getElementById('appointmentDate')
+    const horaCita = document.getElementById('appointmentTime')
+    const motivoConsulta = document.getElementById('appointmentReason')
+    const especialidad = document.getElementById('specialty')
+    const drAsignado = document.getElementById('doctor')
+    const tipoConsulta = document.getElementById('appointmentType')
+    //ARTURO FIN
+
     form.addEventListener('submit' , (e) => {
 
     e.preventDefault()
@@ -23,6 +32,17 @@ const validarCampos = () => {
     const patientGenderValor= patientGender.value.trim()
     const patientPhoneValor = patientPhone.value.trim()
     const emailValor = email.value.trim()
+
+    //ARTURO
+
+    const fechaCitaValor = fechaCita.value.trim()
+    const horaCitaValor = horaCita.value.trim()
+    const motivoConsultaValor = motivoConsulta.value.trim()
+    const especialidadValor = especialidad.value.trim()
+    const drAsignadoValor = drAsignado.value.trim()
+    const tipoConsultaValor = tipoConsulta.value.trim()
+
+    // ARTURO FIN
 
     if(!patientNameValor){
         validaFalla(patientName, 'Campo Vacio, Nombre Completo');return false;
@@ -67,6 +87,51 @@ const validarCampos = () => {
         validaOk(email)
     }
 
+     // ARTURO
+    let minQuinceCaracteres = /^.{15,}$/.test(motivoConsultaValor)
+
+    if (!fechaCitaValor) {
+        validaFalla(fechaCita, 'Campo Vacio, Fecha de la cita')
+    } else if (!validarFecha(fechaCitaValor)) {
+        validaFalla(fechaCita, 'Error En el formato')
+    } else {
+        validaOk(fechaCita) // FORMATO 2024-09-20
+    }
+
+    if (!horaCitaValor){
+        validaFalla(horaCita, 'Campo Vacio, Hora de la cita')
+    }else{
+        validaOk(horaCita)
+    }
+
+    if (!motivoConsultaValor){
+        validaFalla(motivoConsulta, 'Campo Vacio, Motivo de la cita')
+    }else if(!minQuinceCaracteres){
+        validaFalla(motivoConsulta, 'Debe especificar mejor el motivo de la consulta.')
+    }else{
+        validaOk(motivoConsulta)
+    }
+
+    if(especialidadValor==0){
+        validaFalla(especialidad, 'Campo Vacio, Especialista de la cita')
+    }else{
+        validaOk(especialidad)
+    }
+
+    if(!drAsignadoValor){
+        validaFalla(drAsignado, 'Campo vacio, Doctor de la cita')
+    }else{
+        validaOk(drAsignado)
+    }
+
+    if(tipoConsultaValor==0){
+        validaFalla(tipoConsulta, 'Campo Vacio, Consultorio de la cita')
+    }else {
+        validaOk(tipoConsulta)
+    }
+    
+    // ARTURO FIN
+
 }   
     const validaFalla = (input, msje) => {
         const formControl = input.parentElement
@@ -91,6 +156,12 @@ const validarCampos = () => {
         if (key < 48 || key > 57) {
           e.preventDefault();
         }
+    }
+
+// ARTURO
+    const validarFecha = (fecha) => {
+        const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+        return regex.test(fecha)
     }
 
 
